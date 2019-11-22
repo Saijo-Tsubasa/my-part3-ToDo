d71863c8ed85d29c4e1333360191c74c668ef0a0
Saijo-Tsubasa <saijo.miraidenshi@gmail.com>
2019-11-22 22:23:42 +0900

    first commit

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,11 @@ app_name='app'
 urlpatterns = [
         path('', views.index, name='index'),
         path('users/<int:pk>/', views.user_detail, name='user_detail'),
+        path('photos/new/', views.photos_new, name='photos_new'),
+        path('photos/<int:pk>/', views.photos_detail, name='photos_detail'),
+        path('photos/<int:pk>/delete/', views.photos_delete, name='photos_delete'),
+        path('photos/<str:category>/', views.photos_category, name='photos_category'),
+        path('signup/', views.signup, name='signup'),
         path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
         path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
-from .models import Photo
+from .models import Photo, Category
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import PhotoForm
+from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
@@ -11,3 +17,48 @@ def user_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
     photos = user.photo_set.all().order_by('-created_at')
     return render(request, 'app/user_detail.html', {'user':user, 'photos':photos})
+
+def signup(request):
+    if request.method=="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            input_username = form.cleaned_data['username']
+            input_password = form.cleaned_data['password1']
+            new_user = authenticate(username=input_username, password=input_password)
+            if new_user is not None:
+                login(request, new_user)
+                return redirect('app:user_detail', pk=new_user.pk)
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/signup.html', {'form':form})
+
+@login_required
+def photos_new(request):
+    if request.method=='POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.user = request.user
+            photo.save()
+            messages.success(request, '投稿が完了しました')
+        return redirect('app:user_detail', pk=request.user.pk)
+    else:
+        form = PhotoForm()
+    return render(request, 'app/photos_new.html', {'form':form})
+
+def photos_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'app/photos_detail.html', {'photo':photo})
+
+def photos_category(request, category):
+    category = Category.objects.get(title=category)
+    photos = Photo.objects.filter(category=category).order_by('-created_at')
+    return render(request, 'app/index.html', {'photos':photos, 'category':category})
+
+
+@require_POST
+def photos_delete(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    photo.delete()
+    return redirect('app:user_detail', request.user.id)
